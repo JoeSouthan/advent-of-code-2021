64fafb92646f74650bc7f3f2eec75a917d251b9c
Joseph Southan <joseph@southanuk.co.uk>
2021-12-03 23:08:43 +0000

    Add template

---
+++ _template/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	utils "advent-of-code/utils/files"
+	"fmt"
+	"os"
+)
+
+func part1(lines []string) {
+}
+
+func part2(lines []string) {
+}
+
+func main() {
+	path := os.Args[1]
+	lines, err := utils.ParseInput(path)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Part 1")
+	part1(lines)
+	fmt.Println("Part 1")
+	part2(lines)
+}
